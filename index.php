b0ea9fb574b79113e0c1632ba4290bd966e1a7fb
alerafart <maalejandrarafart@gmail.com>
2022-03-18 19:26:06 +0100

    formulaire fini avec envoi de mail operationel

--- index.php
+++ index.php
@@ -2,32 +2,64 @@
 
 $firstName = $name = $email = $phone = $message = "";
 $firstNameError= $nameError= $emailError=$phoneError = $messageError = "";
+$isSuccess = false;
+$emailTo = "ale_rafart@hotmail.com";
+
 if ($_SERVER["REQUEST_METHOD"]== "POST"){
     $firstName= verifyInput($_POST["firstname"]) ;
     $name= verifyInput($_POST["name"]);
     $email= verifyInput($_POST["email"]);
     $phone= verifyInput($_POST["phone"]);
     $message= verifyInput($_POST["message"]);
+    $isSuccess = true;
+    $emailText = "";
 
     if(empty($firstName)){
         $firstNameError="Je veux connaître ton prénom!";
+        $isSuccess = false;
+    }
+    else{
+        $emailText .= "FirstName: $firstName\n";
     }
 
     if(empty($name)){
         $nameError="Et oui je veux tout savoir, même ton nom!";
+        $isSuccess = false;
+    }
+    else{
+        $emailText .= "Name: $name\n";
+    }
+
+    if(!isEmail($email)){
+        $emailError = "ce n'est pas un mail valide";
+        $isSuccess = false;
+    }
+    else{
+        $emailText .= "Email : $email\n";
     }
 
     if(!isPhone($phone)){
         $phoneError="Que des chiffres stp, 10 attendues";
+        $isSuccess = false;
+    }
+    else{
+        $emailText .= "Phone: $phone\n";
     }
 
     if(empty($message)){
         $messageError= "tu n'as rien a me dire? :-(";
+        $isSuccess = false;
+    }
+    else{
+        $emailText .= "Message: $message\n";
     }
 
     
-    if(!isEmail($email)){
-        $emailError = "ce n'est pas un mail valide";
+    if ($isSuccess){
+        //envoyer
+        $headers= "From: $firstName $name <$email>\r\nReply-To: $email";
+        mail($emailTo, "Un message de votre formulaire", $emailText, $headers);
+        $firstName = $name = $email = $phone = $message = "";
     }
 }
 
@@ -109,7 +141,7 @@ function verifyInput($var){
                                 <input type="submit" class="button1" value="Envoyer">
                             </div>
 
-                            <p class="thank-you">Votre message a bien été envoyé. Merci de m'avoir contactée</p>
+                            <p class="thank-you" style="display:<?php if($isSuccess) echo 'block'; else echo 'none'; ?>">Votre message a bien été envoyé, merci de m'avoir contactée :)</p>
                         </div>
                         
 
